29ef3303080994a57b51aeec35e62e041270b758
Thang19it3 <ngovanthang1004@gmail.com>
2024-11-05 23:03:36 +0700

    Add-info

--- FE/dau-gia-truc-tuyen/src/queries/AdminAPI.ts
+++ FE/dau-gia-truc-tuyen/src/queries/AdminAPI.ts
@@ -92,17 +92,8 @@ export const getWard = async () => {
 
 export const addUserInformation = async (formData: FormData) => {
     try {
-        const token = localStorage.getItem('token');
-
-        const fullName = formData.get('fullName') as string;
-        const phone = formData.get('phone') as string;
-        const city = formData.get('city') as string;
-        const ward = formData.get('ward') as string;
-        const district = formData.get('district') as string;
-        const address = formData.get('address') as string;
-        console.log(formData.get('frontCCCD'));
-        console.log(formData.get('avatar'));
-        const response = await axiosInstance.put(`api/UserOrAdmin/addInformation?fullName=${fullName}&phone=${phone}&city=${city}&ward=${ward}&district=${district}&address=${address}`, formData, {
+        const token = localStorage.getItem('token');    
+        const response = await axiosInstance.put('api/UserOrAdmin/addInformation', formData, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
@@ -128,3 +119,32 @@ export const profileUser = async () => {
         throw new Error('Error fetching user profile'); 
     }
 };
+
+export const listOfRegisteredbidders = async () => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axiosInstance.get('/api/auction/listofregisteredbidders', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.data.result;
+    } catch (error) {
+        
+    }
+}
+
+export const joinRoomAuction = async (id: string) => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await axiosInstance.get(`/api/joinRoomAuction?id=${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+
+        return response.data.result;
+    } catch (error) {
+        
+    }
+}
